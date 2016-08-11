584a35cd67514a1a41cd5f99267b741ea85d3669
Henon W. Chesser <henonchesser@yahoo.com>
2016-08-11 12:04:10 -0400

    added grassy effect

---
+++ Assets/Scripts/TerrainEffectGrassy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainEffectGrassy : MonoBehaviour {
+
+    public GameObject _bladeOfGrass;
+
+	// Use this for initialization
+	void Start () {
+	    for ( var i = 0; i < 40; i++)
+        {
+            var randomPos = transform.position;
+            randomPos.x += Random.Range(-0.5f, 0.5f);
+            randomPos.z += Random.Range(-0.5f, 0.5f);
+            randomPos.y -= .375f;
+
+            var newBlade = (GameObject)Instantiate(_bladeOfGrass, randomPos, Quaternion.identity);
+            newBlade.transform.parent = transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
