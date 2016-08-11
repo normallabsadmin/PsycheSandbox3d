4f980286511b91fa8bf86a3f665c872b52d21c2f
Henon W. Chesser <henonchesser@yahoo.com>
2016-08-11 17:30:08 -0400

    gave entitys mock shadows

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Tree : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private GameObject _mySprite;
+    private GameObject _myShadow;
+
+    private void Start()
+    {
+       
+        _mySprite = transform.FindChild("Sprite").gameObject;
+        _myShadow = transform.FindChild("Shadow").gameObject;
+    }
+
+    private void RandomNudge()
+    {
+
+    }
 }
