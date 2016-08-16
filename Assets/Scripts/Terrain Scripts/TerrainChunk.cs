57c6e2a5794e5ee39d6b6ca6763356ec6a2c5fc7
Henon W. Chesser <henonchesser@yahoo.com>
2016-08-16 16:59:59 -0400

    created terrain chunker varaibles

--- Assets/Scripts/Terrain Scripts/TerrainChunk.cs
+++ Assets/Scripts/Terrain Scripts/TerrainChunk.cs
@@ -4,44 +4,81 @@ using System.Collections;
 public class TerrainChunk : MonoBehaviour {
 
     public Texture2D _layoutTexture;
-    public GameObject _demoCube;
+    public int _elevationBump = 0;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject _demoCube;
+    public GameObject _grassBlock;
+    public GameObject _dirtBlock;
+    public GameObject _waterBlock;
+    public GameObject _sandBlock;
+    public GameObject _stoneBlock;
+
+    // Use this for initialization
+    void Start () {
        
         int i, j;
+        float x = -5;
+        float z = -5;
 
 	    if (_layoutTexture != null)
         {
             for ( i = 0; i < 10; i++)// on row 
             {
                 for ( j = 0; j < 10; j++) //on column
-                {
-                    
-                    //var thisPixel = _layoutTexture.GetPixel(j,i);
-                    var x = (i * 10);
-                    var y = (j * 10);
-                    Debug.Log("X:" + x + " | Y:" + y);
-                    
-                    var thisPixelGroup = _layoutTexture.GetPixels(x, y, 10, 10);
-                    var thisPixel = thisPixelGroup[10];
-                    //Debug.Log(thisPixel);
-                   
-                    var newCube = (GameObject)Instantiate(_demoCube, transform.position, Quaternion.identity);
-                    newCube.GetComponent<Renderer>().material.color = thisPixel;
-                    newCube.transform.parent = transform;
-                    newCube.transform.position = new Vector3(i, 1, j);
-                    
+                { 
+                    var thisPixel = _layoutTexture.GetPixel(i, j);
+                    SpawnTerrain(thisPixel,x,z);
+                    x++;
+                    #region debugCode
+                        /*
+                         * this code was used to test reading sprites
+                          Debug.Log(thisPixel);
+                        var newCube = (GameObject)Instantiate(_demoCube, transform.position, Quaternion.identity);
+                        newCube.GetComponent<Renderer>().material.color = thisPixel;
+                        newCube.transform.parent = transform;
+                        newCube.transform.position = new Vector3(i, 1, j);
 
+                        var hex = ColorUtility.ToHtmlStringRGBA(thisPixel);
+                        if(hex == "C412FFFF")
+                        {
+                            Debug.Log("Win!");
+                        }
+                        */
+                        #endregion
                 }
+                x = -5;
                 j = 0;
+                z++;
             }
-           
         }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void SpawnTerrain(Color pixel, float x, float z)
+    {
+        var hexString = ColorUtility.ToHtmlStringRGBA(pixel);
+
+        GameObject terrainToSpawn;
+        float heightAdjust;
+        bool hasTree = false;
+        Vector3 posTerrain = new Vector3(x, 1, z);
+
+        switch (hexString)
+        {
+            case "C412FFFF ":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = -.5;
+                hasTree = true;
+                break;
+            default:
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 0;
+                hasTree = false;
+                break;
+
+        }
+
+        var newTerrain = (GameObject)Instantiate(terrainToSpawn, posTerrain, Quaternion.identity);
+        var terrainComp = newTerrain.GetComponent<Terrain>();
+        terrainComp._hasTree = hasTree;
+    }
 }
