40b3bd66f538763291c084b911c9f5a153530c0e
Henon W. Chesser <henonchesser@yahoo.com>
2016-08-16 17:11:13 -0400

    finished chunker reader

--- Assets/Scripts/Terrain Scripts/TerrainChunk.cs
+++ Assets/Scripts/Terrain Scripts/TerrainChunk.cs
@@ -64,13 +64,164 @@ public class TerrainChunk : MonoBehaviour {
 
         switch (hexString)
         {
-            case "C412FFFF ":
+            //grass with tree
+            //low gwt
+            case "C412FFFF":
                 terrainToSpawn = _grassBlock;
-                heightAdjust = -.5;
+                heightAdjust = -0.5f;
                 hasTree = true;
                 break;
-            default:
+           //flat gwt
+            case "B200FFff":
                 terrainToSpawn = _grassBlock;
+                heightAdjust = 0f;
+                hasTree = true;
+                break;
+            //high gwt
+            case "9300E0FF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 0.5f;
+                hasTree = true;
+                break;
+            //next level gwt
+            case "480095FF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 1f;
+                hasTree = true;
+                break;
+
+            //grass
+            //low grass
+            case "179625FF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = -0.5f;
+                hasTree = false;
+                break;
+            //flat grass
+            case "007F0EFF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 0f;
+                hasTree = false;
+                break;
+            //high grass
+            case "005100FF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 0.5f;
+                hasTree = false;
+                break;
+            //next level grass
+            case "002900FF":
+                terrainToSpawn = _grassBlock;
+                heightAdjust = 1f;
+                hasTree = false;
+                break;
+
+            //water
+            //low water
+            case "173DFFFF":
+                terrainToSpawn = _waterBlock;
+                heightAdjust = -0.5f;
+                hasTree = false;
+                break;
+            //flat water
+            case "0026FFFF":
+                terrainToSpawn = _waterBlock;
+                heightAdjust = 0f;
+                hasTree = false;
+                break;
+            //high water
+            case "0000D1FF":
+                terrainToSpawn = _waterBlock;
+                heightAdjust = 0.5f;
+                hasTree = false;
+                break;
+            //next level water
+            case "0000A9FF":
+                terrainToSpawn = _waterBlock;
+                heightAdjust = 1f;
+                hasTree = false;
+                break;
+
+            //stone
+            //low stone
+            case "979797FF":
+                terrainToSpawn = _stoneBlock;
+                heightAdjust = -0.5f;
+                hasTree = false;
+                break;
+            //flat stone
+            case "808080FF":
+                terrainToSpawn = _stoneBlock;
+                heightAdjust = 0f;
+                hasTree = false;
+                break;
+            //high stone
+            case "525252FF":
+                terrainToSpawn = _stoneBlock;
+                heightAdjust = 0.5f;
+                hasTree = false;
+                break;
+            //next level stone
+            case "2A2A2AFF":
+                terrainToSpawn = _stoneBlock;
+                heightAdjust = 1f;
+                hasTree = false;
+                break;
+
+            //sand
+            //low sand
+            case "FFEF17FF":
+                terrainToSpawn = _sandBlock;
+                heightAdjust = -0.5f;
+                hasTree = false;
+                break;
+            //flat sand
+            case "FFD800FF":
+                terrainToSpawn = _sandBlock;
+                heightAdjust = 0f;
+                hasTree = false;
+                break;
+            //high sand
+            case "D1AA00FF":
+                terrainToSpawn = _sandBlock;
+                heightAdjust = 0.5f;
+                hasTree = false;
+                break;
+            //next level sand
+            case "916A00FF":
+                terrainToSpawn = _sandBlock;
+                heightAdjust = 1f;
+                hasTree = false;
+                break;
+
+            //dirt
+            //low dirt
+            case "A65A27FF":
+                terrainToSpawn = _dirtBlock;
+                heightAdjust = -0.5f;
+                hasTree = false;
+                break;
+            //flat dirt
+            case "7F3300FF":
+                terrainToSpawn = _dirtBlock;
+                heightAdjust = 0f;
+                hasTree = false;
+                break;
+            //high dirt
+            case "621600FF":
+                terrainToSpawn = _dirtBlock;
+                heightAdjust = 0.5f;
+                hasTree = false;
+                break;
+            //next level dirt
+            case "220000FF":
+                terrainToSpawn = _dirtBlock;
+                heightAdjust = 1f;
+                hasTree = false;
+                break;
+
+            default:
+                terrainToSpawn = _dirtBlock;
                 heightAdjust = 0;
                 hasTree = false;
                 break;
